553571c5c783af878e7387d68f0cbfd6edae74fe
Omar Roa <oeroaq@unal.edu.co>
2017-08-26 17:41:22 +0000

    arreglando errores codigo estudiantes

--- app/models/estudiante.rb
+++
@@ -1,2 +0,0 @@
-class Estudiante < ApplicationRecord
-end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,5 @@ Rails.application.routes.draw do
   resources :publicacions
   resources :users
   resources :universidads
-  resources :estudiantes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
