926a61c19ff8d9e2ae043565293198b5f79ea943
Omar Roa <oeroaq@unal.edu.co>
2017-08-26 17:35:35 +0000

    arreglando arrores codigo estudiantes

--- db/migrate/20170824203828_estudiante.rb
+++ db/migrate/20170824203828_estudiante.rb
@@ -1,9 +1,11 @@
-class Estudiante < ActiveRecord::Migration[5.1]
+class CreateEstudiantes < ActiveRecord::Migration[5.1]
   def change
     create_table :estudiantes do |t|
       t.text :Nombre
       t.string :Carrera
       t.integer :Codigo
+
       t.timestamps
+    end
   end
 end
